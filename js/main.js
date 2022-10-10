24fc761e45594b2eb6569b50208b26657087b070
ValenCh <107009740+ValenCh@users.noreply.github.com>
2022-10-10 02:45:12 -0300

    segunda entrega del proyecto final

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
 const autos = [
-    {id: 0, nombre: "Fiat Cronos", marca: "Fiat", precio: 3177000, cajaDeCambios: "automatica", puertas: 5, caballosDeFuerza: 130, estado: "0 km" },
-    {id: 1, nombre: "Toyota Hilux", marca: "Toyota", precio: 7777000, cajaDeCambios: "automatica", puertas: 4, caballosDeFuerza: 180, estado: "0 km" },
-    {id: 2, nombre: "Corolla GR-Sport", marca: "Toyota", precio: 6647000, cajaDeCambios: "automatica", puertas: 5, caballosDeFuerza: 170, estado: "0 km" },
-    {id: 3, nombre: "Toyota Yairis", marca: "Toyota", precio: 3432000, cajaDeCambios: "manual", puertas: 5, caballosDeFuerza: 125, estado: "0 km" },
-    {id: 4, nombre: "Renault Sandero", marca: "Renault", precio: 3242000, cajaDeCambios: "automatica", puertas: 5, caballosDeFuerza: 145, estado: "0 km" },
-    {id: 5, nombre: "Ford Ranger", marca: "Ford", precio: 6137000, cajaDeCambios: "manual", puertas: 4, caballosDeFuerza: 160, estado: "0 km" },
-    {id: 6, nombre: "Gol Trend", marca: "Volkswagen", precio: 2849900, cajaDeCambios: "manual", puertas: 5, caballosDeFuerza: 101, estado: "usado" },
-    {id: 7, nombre: "Fiat Palio", marca: "Fiat", precio: 1980000, cajaDeCambios: "manual", puertas: 5, caballosDeFuerza: 70, estado: "usado" },
-    {id: 8, nombre: "Ford Fiesta", marca: "Ford", precio: 3699000, cajaDeCambios: "manual", puertas: 5, caballosDeFuerza: 120, estado: "usado" },
-    {id: 9, nombre: "Renault Clio", marca: "Renault", precio:  1650000, cajaDeCambios: "manual", puertas: 5, caballosDeFuerza: 85, estado: "usado" },
-    {id: 10, nombre: "Chevrolet Corsa Classic", marca: "Chevrolet", precio: 1449000, cajaDeCambios: "manual", puertas: 5, caballosDeFuerza: 92, estado: "usado" },
-    {id: 11, nombre: "Mercedes-Benz GLA-Class", marca: "Mercedes-Benz", precio: 3699000, cajaDeCambios: "automatica", puertas: 5, caballosDeFuerza: 136, estado: "usado" }
+    {id: 0, nombre: "Fiat Cronos", marca: "fiat", precio: 3177000, cajaDeCambios: "automatica", puertas: 5, caballosDeFuerza: 130, estado: "0km", cantidad: 2},
+    {id: 1, nombre: "Toyota Hilux", marca: "toyota", precio: 7777000, cajaDeCambios: "automatica", puertas: 4, caballosDeFuerza: 180, estado: "0km", cantidad: 1},
+    {id: 2, nombre: "Corolla GR-Sport", marca: "toyota", precio: 6647000, cajaDeCambios: "automatica", puertas: 5, caballosDeFuerza: 170, estado: "0km", cantidad: 1},
+    {id: 3, nombre: "Toyota Yairis", marca: "toyota", precio: 3432000, cajaDeCambios: "manual", puertas: 5, caballosDeFuerza: 125, estado: "0km", cantidad: 2},
+    {id: 4, nombre: "Renault Sandero", marca: "renault", precio: 3242000, cajaDeCambios: "automatica", puertas: 5, caballosDeFuerza: 145, estado: "0km", cantidad: 3},
+    {id: 5, nombre: "Ford Ranger", marca: "ford", precio: 6137000, cajaDeCambios: "manual", puertas: 4, caballosDeFuerza: 160, estado: "0km", cantidad: 2},
+    {id: 6, nombre: "Gol Trend", marca: "volkswagen", precio: 2849900, cajaDeCambios: "manual", puertas: 5, caballosDeFuerza: 101, estado: "usado", cantidad: 1},
+    {id: 7, nombre: "Fiat Palio", marca: "fiat", precio: 1980000, cajaDeCambios: "manual", puertas: 5, caballosDeFuerza: 70, estado: "usado", cantidad: 1},
+    {id: 8, nombre: "Ford Fiesta", marca: "ford", precio: 3699000, cajaDeCambios: "manual", puertas: 5, caballosDeFuerza: 120, estado: "usado", cantidad: 2},
+    {id: 9, nombre: "Renault Clio", marca: "renault", precio:  1650000, cajaDeCambios: "manual", puertas: 5, caballosDeFuerza: 85, estado: "usado", cantidad: 1},
+    {id: 10, nombre: "Chevrolet Corsa Classic", marca: "chevrolet", precio: 1449000, cajaDeCambios: "manual", puertas: 5, caballosDeFuerza: 92, estado: "usado", cantidad: 1},
+    {id: 11, nombre: "Mercedes-Benz GLA-Class", marca: "mercedesbenz", precio: 3699000, cajaDeCambios: "automatica", puertas: 5, caballosDeFuerza: 136, estado: "usado", cantidad: 1}
 ]
 
-const carrito = document.getElementById("carrito")
-
+const carrito = document.getElementById("carrito");
+const buscar = document.getElementById("busqueda");
 
 function carritoDeCompra(){
    
@@ -39,7 +39,7 @@ function carritoDeCompra(){
     let ingreseAuto;
     let auto;
     let n = "no";
-    let precioAuto;
+    let x;
  
     while(n != "si"){
      ingreseAuto = prompt("Ingrese el auto que quiera reservar (copie y pegue el nombre del auto desde nuestro catalogo)");
@@ -47,63 +47,63 @@ function carritoDeCompra(){
  
      switch(auto){
           case "fiatcronos":
-            aElegido = autos[0];
-         n = prompt(`El Fiat Cronos vale $${autos[0].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
+         x = autos[0].id;
+         n = prompt(`El Fiat Cronos vale $${autos[x].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
          break;
          
          case "toyotahilux":
-            aElegido = autos[1];
-         n = prompt(`El Toyota Hilux vale $${autos[1].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
+         x = autos[1].id;
+         n = prompt(`El Toyota Hilux vale $${autos[x].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
          break;
          
          case "corollagr-sport":
-            aElegido = autos[2];
-         n = prompt(`El Corolla GR-Sport vale $${autos[2].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
+         x = autos[2].id;
+         n = prompt(`El Corolla GR-Sport vale $${autos[x].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
          break;
          
          case "toyotayaris":
-            aElegido = autos[3];
-         n = prompt(`El Toyota Yaris vale $${autos[3].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
+         x = autos[3].id;
+         n = prompt(`El Toyota Yaris vale $${autos[x].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
          break;
          
          case "renaultsandero":
-            aElegido = autos[4];
-         n = prompt(`El Renault Sandero vale $${autos[4].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
+         x = autos[4].id;
+         n = prompt(`El Renault Sandero vale $${autos[x].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
          break;
          
          case "fordranger":
-            aElegido = autos[5];
-         n = prompt(`El Ford Ranger vale $${autos[5].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
+         x = autos[5].id;
+         n = prompt(`El Ford Ranger vale $${autos[x].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
          break;
  
          case "vwgolytrend":
-            aElegido = autos[6];
-         n = prompt(`El VW Gol y Trend vale $${autos[6].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
+         x = autos[6].id;
+         n = prompt(`El VW Gol y Trend vale $${autos[x].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
          break;
  
          case "fiatpalio":
-            aElegido = autos[7];
-         n = prompt(`El Fiat Palio vale $${autos[7].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
+         x = autos[7].id;
+         n = prompt(`El Fiat Palio vale $${autos[x].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
          break;
          
          case "fordfiesta":
-            aElegido = autos[8];
-         n = prompt(`El Ford Fiesta vale $${autos[8].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
+         x = autos[8].id;
+         n = prompt(`El Ford Fiesta vale $${autos[x].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
          break;
          
          case "renaultclio":
-            aElegido = autos[9];
-         n = prompt(`El Renault Clio $${autos[9].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
+         x = autos[9].id;
+         n = prompt(`El Renault Clio $${autos[x].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
          break;
          
          case "chev.corsayclassic":
-            aElegido = autos[10];
-         n = prompt(`El Chev. Corsa y Classic vale $${autos[10].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
+         x = autos[10].id;
+         n = prompt(`El Chev. Corsa y Classic vale $${autos[x].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
          break;
          
          case "mercedes-benzgla-class":
-            aElegido = autos[11];
-         n = prompt(`El Mercedes-Benz GLA-Class vale $${autos[11].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
+         x = autos[11].id;
+         n = prompt(`El Mercedes-Benz GLA-Class vale $${autos[x].precio}, para reservarlo ponga 'si', sino ingrese 'no' `);
          break;
          
          default:
@@ -121,18 +121,107 @@ function carritoDeCompra(){
     let pedirDescuento = prompt("Si tiene ingrese su codigo de descuento").toLowerCase().split(' ').join('');
     
     if(pedirDescuento == codigoDeDescuento){
-     aElegido.precio =  descuento(aElegido.precio, 10);
+     autos[x].precio =  descuento(autos[x].precio, 10);
     } else if(pedirDescuento == codigoDeDescuento2){
-     precioAuto = descuento(aElegido.precio, 5);
+     autos[x].precio = descuento(autos[x].precio, 5);
     }else{
      alert("puedes conseguir el descuento viendo el codigo 😁");
     }
  
-    alert(`${nombre} ${apellido} usted ha seleccionado el ${aElegido.nombre}, cuyo precio final es de ${aElegido.precio}. Nosotros nos comunicaremos al ${numeroDeTelefono} para terminar con los ultimos detalles de su compra. ¡Gracias por elegirnos!`);
+    alert(`${nombre} ${apellido} usted ha seleccionado el ${autos[x].nombre}, cuyo precio final es de ${autos[x].precio}. Nosotros nos comunicaremos al ${numeroDeTelefono} para terminar con los ultimos detalles de su compra. ¡Gracias por elegirnos!`);
     
     
 }
 
-carrito.onclick = () => {
+carrito.onclick = () =>{
     carritoDeCompra();
+}
+
+
+function busqueda(){
+    let qBusco = prompt("Que quieres saber? (marca?, precio? caja de cambios?, puertas?, caballos de fuerza?, estado?)").toLowerCase().split(' ').join('');
+    let busquedaUsuario;
+    let resultado = [];
+
+    
+    switch(qBusco){
+        case "marca":
+            busquedaUsuario = prompt("Que marca de auto buscas?").toLowerCase().split(' ').join('');
+            for(let i = 0; i < autos.length; i++){
+                if(autos[i].marca == busquedaUsuario){
+                    resultado.push(autos[i].nombre)
+                }
+            }
+        break;
+
+        case "precio":
+            busquedaUsuario = Number(prompt("Quiero que el auto valga menos que... (el minimo es 1449000)"));
+            for(let i = 0; i < autos.length; i++){
+                if(autos[i].precio <= busquedaUsuario){
+                    resultado.push(autos[i].nombre)
+                }
+            }
+        break;
+
+        case "cajadecambios":
+            busquedaUsuario = prompt("Que caja quieres: manual o automatica?").toLocaleLowerCase();
+            for(let i = 0; i < autos.length; i++){
+                if(autos[i].cajaDeCambios == busquedaUsuario){
+                    resultado.push(autos[i].nombre)
+                }
+            }
+        break;
+
+        case "puertas":
+            busquedaUsuario = Number(prompt("Quiero que el auto tenga esta cantidad de puertas"));
+            for(let i = 0; i < autos.length; i++){
+                if(autos[i].puertas == busquedaUsuario){
+                    resultado.push(autos[i].nombre)
+                }
+            }
+        break;
+
+        case "caballosdefuerza":
+            let minimoOMaxiomo = prompt("Quieres buscar un minimo de cv o un maximo de cv?").toLocaleLowerCase();
+            busquedaUsuario = Number(prompt(`Cuantos CV ${minimoOMaxiomo} quieres?`));
+            if(minimoOMaxiomo == "minimo"){
+                for(let i = 0; i < autos.length; i++){
+                    if(autos[i].caballosDeFuerza >= busquedaUsuario){
+                        resultado.push(autos[i].nombre)
+                    }
+                }
+            }else if(minimoOMaxiomo == "maximo"){
+                for(let i = 0; i < autos.length; i++){
+                    if(autos[i].caballosDeFuerza <= busquedaUsuario){
+                        resultado.push(autos[i].nombre)
+                    }
+                }
+            }else{
+                alert("Escribe solo 'minimo' o 'maxiomo'")
+            }
+        break;
+
+        case "estado":
+                busquedaUsuario = prompt("Quieres un auto '0 km' o un auto 'usado'?").toLowerCase().split(' ').join('');
+            for(let i = 0; i < autos.length; i++){
+                if(autos[i].estado == busquedaUsuario){
+                    resultado.push(autos[i].nombre)
+                }
+            }
+        break;
+
+        default:
+            alert("por favor escribe una de las caracteristicas mencionadas");
+        break;
+            
+
+    }
+
+    alert(`Los autos que cumplen esas caracteristicas son ${resultado.length}: ${resultado}`)
+
+}
+
+buscar.onclick = () =>{
+    alert("No sabes que comprar? Busca alguna caracteristica")
+    busqueda();
 }
